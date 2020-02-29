28682d324e0cb2003bfd95f94712e0ca11b35d3d
Betsycaceres <betsy0357@gmail.com>
2020-02-28 20:36:25 -0500

    creando componentes

---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from "react";
+
+const Footer = () => {
+  return (
+    <footer class="page-footer font-small blue ">
+      <div class="footer-copyright text-center py-3">©2020 Copyright</div>
+    </footer>
+  );
+};
+export default Footer;
---
+++ src/components/header.js
@@ -0,0 +1,6 @@
+import React, { Fragment } from "react";
+
+const Header = () => {
+  return <nav class="navbar navbar-primary bg-primary text-white">Clima</nav>;
+};
+export default Header;
